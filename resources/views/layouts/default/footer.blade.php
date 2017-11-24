2c6cfbfe5a09326a2f1581c5ec8b3e34dbb33b96
Diego Henrique Rodrigues <diego_henriquerodrigues@hotmail.com>
2017-11-24 18:03:59 -0200

    Updated footer

--- resources/views/layouts/default/footer.blade.php
+++ resources/views/layouts/default/footer.blade.php
@@ -1,3 +1,6 @@
-<footer>
-&copy; Copyright - School of Net
+<footer class="page-footer">
+    <div class="container">
+        &copy; Copyright {{ date('Y') }}
+        <a href="#" class="grey-text text-lighten-4 right">Forum</a>
+    </div>
 </footer>
--- resources/views/layouts/default/header.blade.php
+++ resources/views/layouts/default/header.blade.php
@@ -1,1 +1,7 @@
-<h1>My forum</h1>
+<nav>
+    <div class="nav-wrapper">
+        <div class="container">
+            <a href="" class="brand-logo">{{ __('My Heroes - Forum') }}</a>
+        </div>
+    </div>
+</nav>
